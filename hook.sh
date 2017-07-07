7f230aceddab192c3d088fea345cb6a1ca41fdaf
ttionya <git@ttionya.com>
2017-07-07 15:03:43 +0800

    改进 SHELL 脚本，支持拉取新分支

--- hook.sh
+++ hook.sh
@@ -4,4 +4,12 @@
 # $2 项目地址
 # $3 日志地址
 
-git -C $2 checkout $1 >> $3 2>&1 && git -C $2 pull origin $1:$1 >> $3 2>&1
+BRANCH=`git branch | grep -E '^[* ]*$1$' -c`
+
+if [ $BRANCH == 1 ]; then
+    # 存在分支
+    git -C $2 checkout $1 >> $3 2>&1 && git -C $2 pull origin $1:$1 >> $3 2>&1
+else
+    # 新分支
+    git -C $2 pull origin $1:$1 >> $3 2>&1 && git -C $2 checkout $1 >> $3 2>&1
+fi
