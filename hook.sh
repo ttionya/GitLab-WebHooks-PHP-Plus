5946eee73d1b1163b23d2ea42963a4a1a179b2db
ttionya <git@ttionya.com>
2017-07-14 10:47:02 +0800

    修复每次检出都是新分支的问题

--- hook.sh
+++ hook.sh
@@ -4,12 +4,12 @@
 # $2 项目地址
 # $3 日志地址
 
-BRANCH=`git -C $2 branch | grep -E '^[* ]*$1$' -c`
+BRANCH=`git -C $2 branch | grep -E “^[* ]*$1$” -c`
 
-if [ $BRANCH == 1 ]; then
+if [ $BRANCH == ‘1’ ]; then
     echo '存在分支' >> $3
-    git -C $2 checkout $1 >> $3 2>&1 && git -C $2 pull origin $1:$1 >> $3 2>&1
+    git -C $2 checkout $1 >> $3 2>&1 && git -C $2 pull >> $3 2>&1
 else
     echo '新分支' >> $3
-    git -C $2 pull origin $1:$1 >> $3 2>&1 && git -C $2 checkout $1 >> $3 2>&1
+    git -C $2 fetch origin $1 >> $3 2>&1 && git -C $2 checkout $1 >> $3 2>&1
 fi
